9c8bd4f4d2f2cbfa4b2a7ffdb2cfafe78fe2c53c
Truong Minh Tien <145823751+marcus1823@users.noreply.github.com>
2025-07-19 00:02:09 +0700

    fix router subscription

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -58,7 +58,9 @@ export default function TabsLayout() {
                   <View className="flex-row items-center gap-2.5">
                     <TouchableOpacity
                       className="bg-[#1E233A] rounded-full px-4 py-2.5"
-                      onPress={() => { }}
+                      onPress={() => {
+                        router.push("/(tabs)/home/subscription");
+                      }}
                     >
                       <Text className="text-center text-white font-quicksand">
                         Nâng Cấp Gói
